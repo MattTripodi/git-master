a9528ba2c2c1e8cd26e065f2e8b1495102264f6f
Matt Tripodi <mtripodi1027@gmail.com>
2016-12-05 14:29:30 -0500

    Removed didRecieveMemoryWarning

--- GitMaster/ViewController.swift
+++ GitMaster/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet weak var whateverLabel: UILabel!
+	
+	
+	@IBAction func buttonPressed(_ sender: Any) {
+		whateverLabel.text = "Hello there"
+	}
+	
+	@IBAction func whiteButtonPressed(_ sender: Any) {
+		self.view.backgroundColor = UIColor.black
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
 
